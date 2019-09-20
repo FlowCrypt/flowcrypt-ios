257541c80c30df66833448fb4ba526a0b0bc4bec
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2019-09-20 12:53:15 +0200

    Search backups in all folders

--- FlowCrypt/Extensions/Extensions.swift
+++ FlowCrypt/Extensions/Extensions.swift
@@ -162,3 +162,11 @@ extension UITableView {
         return cell
     }
 }
+
+extension Array {
+    func chunked(_ size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
