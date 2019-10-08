4af4d897ced285c5597062ec8c518990b623bdb5
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2019-10-08 23:09:57 +0200

    #119 Fix issues in InboxViewController fetching (#120)
    * Fix issues in InboxViewController fetching
    
    * minor update

--- FlowCrypt/Extensions/UIViewControllerExtension.swift
+++ FlowCrypt/Extensions/UIViewControllerExtension.swift
@@ -173,3 +173,29 @@ extension UIViewController {
         }
     }
 }
+
+extension UINavigationController {
+    func pushViewController(viewController: UIViewController, animated: Bool, completion: @escaping () -> ()) {
+        pushViewController(viewController, animated: animated)
+
+        if let coordinator = transitionCoordinator, animated {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+
+    func popViewController(animated: Bool, completion: @escaping () -> ()) {
+        popViewController(animated: animated)
+
+        if let coordinator = transitionCoordinator, animated {
+            coordinator.animate(alongsideTransition: nil) { _ in
+                completion()
+            }
+        } else {
+            completion()
+        }
+    }
+}
