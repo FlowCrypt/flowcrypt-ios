22c03ba5fc4289125a8781aea861d9d356e9836b
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-01-07 21:34:38 +0100

    remove comments

--- FlowCrypt/Controllers/SearchViewController.swift
+++ FlowCrypt/Controllers/SearchViewController.swift
@@ -312,11 +312,10 @@ private extension UISearchBar {
         if #available(iOS 13.0, *) {
             return searchTextField
         } else {
-            return value(forKey: "_searchField") as? UITextField
-//            return subviews.first?
-//                .subviews
-//                .compactMap { $0 as? UITextField }
-//                .first
+            return subviews.first?
+                .subviews
+                .compactMap { $0 as? UITextField }
+                .first
         }
     }
 }
