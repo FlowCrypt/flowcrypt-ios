9bb5cc90a10e03cf55c0f3234ea4672d9250a4cc
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-01-19 23:08:57 +0100

    disable keyboard

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -1,4 +1,14 @@
-defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool NO
+#defaults write com.apple.iphonesimulator ConnectHardwareKeyboard -bool NO
 #/usr/libexec/PlistBuddy -c "Print :DevicePreferences" ~/Library/Preferences/com.apple.iphonesimulator.plist | perl -lne 'print $1 if /^    (\S*) =/' | while read -r a; do /usr/libexec/PlistBuddy -c "Set :DevicePreferences:$a:ConnectHardwareKeyboard
 #false" ~/Library/Preferences/com.apple.iphonesimulator.plist || /usr/libexec/PlistBuddy -c  "Add :DevicePreferences:$a:ConnectHardwareKeyboard
 #bool false" ~/Library/Preferences/com.apple.iphonesimulator.plist; done
+
+
+/usr/libexec/PlistBuddy \
+                         -c \Print :DevicePreferences\ ~/Library/Preferences/com.apple.iphonesimulator.plist | \
+                         perl -lne 'print $1 if /^    (\\S*) =/' | while read -r a; do /usr/libexec/PlistBuddy \
+                         -c \Set :DevicePreferences:$a:ConnectHardwareKeyboard false\ \
+                         ~/Library/Preferences/com.apple.iphonesimulator.plist \
+                         || /usr/libexec/PlistBuddy \
+                         -c \Add :DevicePreferences:$a:ConnectHardwareKeyboard bool false\ \
+                         ~/Library/Preferences/com.apple.iphonesimulator.plist; done
