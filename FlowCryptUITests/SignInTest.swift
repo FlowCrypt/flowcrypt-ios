390605122ffe0f6ab9d0206513549cdd0d0c95d1
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2020-03-18 18:16:55 +0100

    Add UITest to sent message to recepient without pub key

--- FlowCryptUITests/SignInTest.swift
+++ FlowCryptUITests/SignInTest.swift
@@ -22,11 +22,10 @@ class SignInTest: XCTestCase {
     }
 
     func test_cancel_login() {
-        app.tables/*@START_MENU_TOKEN@*/.buttons["gmail"]/*[[".cells.buttons[\"gmail\"]",".buttons[\"gmail\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            .tap()
+        app.tables/*@START_MENU_TOKEN@*/.buttons["gmail"]/*[[".cells.buttons[\"gmail\"]",".buttons[\"gmail\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         wait(1)
 
-        let cancelButton = gmailAlert().buttons["Cancel"]
+        let cancelButton = gmailAlert.buttons["Cancel"]
         XCTAssert(cancelButton.exists, "Cancel in Alert doesn't exist")
         cancelButton.tap()
 
@@ -39,7 +38,7 @@ class SignInTest: XCTestCase {
         // tap on gmail button
         // MARK: - Google Login
         app.tables/*@START_MENU_TOKEN@*/.buttons["gmail"]/*[[".cells.buttons[\"gmail\"]",".buttons[\"gmail\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        let signInAlert = gmailAlert()
+        let signInAlert = gmailAlert
 
         wait(1)
 
@@ -59,7 +58,7 @@ class SignInTest: XCTestCase {
 
 
         textField.typeText(user.email)
-        let returnButton = goKeyboardButton()
+        let returnButton = goKeyboardButton
         XCTAssert(returnButton.exists, "User keyboard button")
         returnButton.tap()
 
@@ -69,7 +68,7 @@ class SignInTest: XCTestCase {
         let passwordTextField = webView.secureTextFields.firstMatch
         passwordTextField.tap()
         passwordTextField.typeText(user.password)
-        let goButton = goKeyboardButton()
+        let goButton = goKeyboardButton
         XCTAssert(goButton.exists, "Password keyboard button")
         goButton.tap()
 
@@ -80,7 +79,7 @@ class SignInTest: XCTestCase {
 
         // MARK: - Wrong pass phrase
         // enter wrong pass phrase and tap enter
-        let button = goKeyboardButton()
+        let button = goKeyboardButton
 
         app.typeText(user.pass + "wrong")
         button.tap()
@@ -106,17 +105,16 @@ class SignInTest: XCTestCase {
         XCTAssert(app.navigationBars["Inbox"].exists, "Could not login")
 
         // MARK: - Send message
-        sendMessage()
+        sendMessage(to: user.email)
         XCTAssert(app.navigationBars["Inbox"].exists, "Failed state after Sending message")
 
         // MARK: - Check in sent mail box
         menuButton().tap()
-        wait(0.3)
-        app.tables.staticTexts["Sent Mail"].tap()
-        wait(5)
+        tapOnMenu(folder: "Sent Mail")
+        wait(3)
 
         app.tables.cells.otherElements.staticTexts[user.email].firstMatch.tap()
-        wait(7)
+        wait(5)
 
         XCTAssert(app.tables.staticTexts[user.email].exists, "Wrong recipient in sent message")
         XCTAssert(app.tables.staticTexts["Some Subject"].exists, "Wrong subject")
@@ -126,19 +124,17 @@ class SignInTest: XCTestCase {
     func test_move_msg_to_trash() {
         // Move msg to Trash
         wait(2)
-        cells.first?.tap()
-        wait(1)
+        tapOnCell()
+
         app.navigationBars.buttons["Delete"].tap()
         wait(1)
 
         // Verify in Trash
         menuButton().tap()
-        app.tables.staticTexts["Trash"].tap()
-        wait(1)
+        tapOnMenu(folder: "Trash")
         XCTAssert(app.tables.cells.otherElements.staticTexts[user.email].exists, "There is no message in trash")
-        cells.first?.tap()
-        wait(1)
 
+        tapOnCell()
         let buttons = app.navigationBars.buttons
         let backButton = buttons["arrow left c"]
 
@@ -151,25 +147,33 @@ class SignInTest: XCTestCase {
         // Open following first msg
         backButton.tap()
         menuButton().tap()
-        app.tables.staticTexts["Inbox"].tap()
-        wait(1)
-        cells.first?.tap()
-        wait(1)
+        tapOnMenu(folder: "Inbox")
+
+        tapOnCell()
     }
 
     func test_move_msg_to_archive() {
         wait(2)
-        sendMessage()
+        sendMessage(to: user.email)
         app.tables.cells.otherElements.staticTexts[user.email].firstMatch.tap()
         app.navigationBars.buttons["archive"].tap()
         wait(2)
         XCTAssert(app.navigationBars["Inbox"].exists, "Failed in sending message to archive")
 
         menuButton().tap()
-        app.tables.staticTexts["All Mail"].tap()
-        wait(2)
+        tapOnMenu(folder: "All Mail")
+        wait(1)
+
         XCTAssert(app.tables.staticTexts[user.email].exists, "Wrong recipient in sent message")
-        XCTAssert(app.tables.staticTexts["Some Subject"].exists, "Wrong subject") 
+        XCTAssert(app.tables.staticTexts["Some Subject"].exists, "Wrong subject")
+    }
+
+    func test_send_message_no_pub_key() {
+        wait(2)
+        sendMessage(to: "flowcrypt.nopubkey@gmail.com")
+        wait(3)
+        let errorAlert = app.alerts["Error"]
+        XCTAssert(errorAlert.exists)
     }
 }
 
@@ -182,22 +186,7 @@ extension SignInTest {
             .sorted(by: { $0.frame.origin.x > $1.frame.origin.x })
     }
 
-    private func sendMessage() {
-        app.buttons["+"].tap()
-        wait(0.2)
-        app.typeText(user.email)
-        app.tables.textFields["Subject"].tap()
-
-        app.tables.textFields["Subject"].tap()
-        app.typeText("Some Subject")
-        let nextCompose = goKeyboardButton()
-        nextCompose.tap()
-        app.typeText("Some text")
-        app.navigationBars["Inbox"].buttons["android send"].tap()
-        wait(5)
-    }
-
-    private func goKeyboardButton() -> XCUIElement {
+    private var goKeyboardButton: XCUIElement {
         if app.buttons["return"].exists {
             return app.buttons["return"]
         }
@@ -211,7 +200,7 @@ extension SignInTest {
         return app.buttons["Go"]
     }
 
-    private func gmailAlert() -> XCUIElement {
+    private var gmailAlert: XCUIElement {
         Springboard.springboard.alerts.element
     }
 
@@ -219,4 +208,35 @@ extension SignInTest {
         app.navigationBars.buttons["menu icn"]
     }
 
+}
+
+extension SignInTest {
+    private func sendMessage(to recipient: String ) {
+        tapOnCompose()
+        app.typeText(recipient)
+        app.tables.textFields["Subject"].tap()
+
+        app.tables.textFields["Subject"].tap()
+        app.typeText("Some Subject")
+
+        goKeyboardButton.tap()
+        app.typeText("Some text")
+        app.navigationBars["Inbox"].buttons["android send"].tap()
+        wait(5)
+    }
+
+    private func tapOnCompose() {
+        app.buttons["+"].tap()
+        wait(0.2)
+    }
+
+    private func tapOnCell() {
+        cells.first?.tap()
+        wait(0.5)
+    }
+
+    private func tapOnMenu(folder: String) {
+        app.tables.staticTexts[folder].tap()
+        wait(1)
+    }
 }
