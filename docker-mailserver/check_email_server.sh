8956e33a3b9bfcf2d8b6cc613c41af19152976df
Tom J <6306961+tomholub@users.noreply.github.com>
2021-08-01 22:17:14 +0800

    issue #307 use external IMAP server for CI (#424)
    * issue 307 fix CI IMAP mailserver
    
    * update install steps
    
    * docker-compose included
    
    * RM docker-toolbox
    
    * RM docker-machine
    
    * add docker-compose
    
    * docker-machine
    
    * restart vb
    
    * use non-standard ports
    
    * check email server up
    
    * sudo echo fix
    
    * echo fix
    
    * fix cd
    
    * increase sleep
    
    * cat hosts + append
    
    * test run
    
    * pdate test
    
    * install telnet
    
    * sleep 1
    
    Co-authored-by: Tom J <tom@holub.me>

---
+++ docker-mailserver/check_email_server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -uxo pipefail
+
+# this script tests the mailserver, make sure it accepts connections
+# make sure that your /etc/hosts includes line "217.163.30.119 mail"
+
+EMAILPORT=10025
+EMAILHOST=mail
+TMPFILE=/tmp/telnet-output.log
+
+( sleep 1; echo -en "EHLO debug\r\n"; sleep 1; ) | telnet $EMAILHOST $EMAILPORT > $TMPFILE
+
+set +x
+
+if grep -Fxq "220-mail.flowcrypt.test ESMTP" $TMPFILE
+then
+    echo "success - SMTP server at '$EMAILHOST' port '$EMAILPORT' is running"
+else
+    echo "failed to confirm that SMTP server is running at '$EMAILHOST' port '$EMAILPORT':"
+    cat $TMPFILE
+    exit 1
+fi
