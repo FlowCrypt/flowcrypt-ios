1036ed4a2b1fae0889a64f25fc56d655ec08c462
Ivan Pizhenko <Ivan.Pizhenko@users.noreply.github.com>
2021-10-13 20:36:09 +0300

    issue #619 Add .pgp to encrypted attachment names

--- Core/source/mobile-interface/endpoints.ts
+++ Core/source/mobile-interface/endpoints.ts
@@ -62,7 +62,7 @@ export class Endpoints {
       const encryptedAtts: Att[] = [];
       for (const att of req.atts || []) {
         const encryptedAtt = await PgpMsg.encrypt({ pubkeys: req.pubKeys, data: Buf.fromBase64Str(att.base64), filename: att.name, armor: false }) as OpenPGP.EncryptBinaryResult;
-        encryptedAtts.push(new Att({ name: att.name, type: 'application/pgp-encrypted', data: encryptedAtt.message.packets.write() }))
+        encryptedAtts.push(new Att({ name: `${att.name}.pgp`, type: 'application/pgp-encrypted', data: encryptedAtt.message.packets.write() }))
       }
       const encrypted = await PgpMsg.encrypt({ pubkeys: req.pubKeys, data: Buf.fromUtfStr(req.text), armor: true }) as OpenPGP.EncryptArmorResult;
       return fmtRes({}, Buf.fromUtfStr(await Mime.encode({ 'text/plain': encrypted.data }, mimeHeaders, encryptedAtts)));
