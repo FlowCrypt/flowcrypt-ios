d7a658648cce97b64edb33fd13f660b6e0649772
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2021-10-27 00:12:55 +0300

    issue #714 pubkey sort pr updates (#791)
    * issue #714 pr updates
    
    * issue #714 update pub keys sorting
    
    Co-authored-by: Tom <tom@flowcrypt.com>

--- FlowCryptAppTests/Functionality/Services/Key Services/Models/RecipientTests.swift
+++ FlowCryptAppTests/Functionality/Services/Key Services/Models/RecipientTests.swift
@@ -13,7 +13,7 @@ class RecipientTests: XCTestCase {
     private let calendar = Calendar.current
 
     func testRecipientWithRevokedKey() {
-        let keyDetails = generateKey(expiration: nil, revoked: true)
+        let keyDetails = createFakeKeyDetails(expiration: nil, revoked: true)
         let recipient = RecipientWithSortedPubKeys(email: "test@test.com", keyDetails: [keyDetails])
 
         XCTAssertEqual(recipient.keyState, .revoked)
@@ -21,7 +21,7 @@ class RecipientTests: XCTestCase {
 
     func testRecipientWithExpiredKey() {
         let expiration = Date().timeIntervalSince1970 - 60 * 60
-        let keyDetails = generateKey(expiration: Int(expiration), revoked: false)
+        let keyDetails = createFakeKeyDetails(expiration: Int(expiration), revoked: false)
 
         let recipient = RecipientWithSortedPubKeys(email: "test@test.com", keyDetails: [keyDetails])
         XCTAssertEqual(recipient.keyState, .expired)
@@ -29,11 +29,11 @@ class RecipientTests: XCTestCase {
 
     func testRecipientWithValidKey() {
         let expiration = Date().timeIntervalSince1970 + 60 * 60
-        let keyDetails = generateKey(expiration: Int(expiration), revoked: false)
+        let keyDetails = createFakeKeyDetails(expiration: Int(expiration), revoked: false)
         let recipient = RecipientWithSortedPubKeys(email: "test@test.com", keyDetails: [keyDetails])
         XCTAssertEqual(recipient.keyState, .active)
 
-        let keyDetails2 = generateKey(expiration: nil, revoked: false)
+        let keyDetails2 = createFakeKeyDetails(expiration: nil, revoked: false)
         let recipient2 = RecipientWithSortedPubKeys(email: "test@test.com", keyDetails: [keyDetails2])
         XCTAssertEqual(recipient2.keyState, .active)
     }
@@ -45,23 +45,23 @@ class RecipientTests: XCTestCase {
 
     func testRecipientKeysOrder() {
         let now = Int(Date().timeIntervalSince1970)
-        let revokedKey = generateKey(expiration: now + 60 * 60, revoked: true)
+        let revokedKey = createFakeKeyDetails(expiration: now + 1 * 3600, revoked: true)
 
-        let activeKey1 = generateKey(expiration: now + 60 * 60)
-        let activeKey2 = generateKey(expiration: now + 48 * 60 * 60)
-        let activeKey3 = generateKey(expiration: now + 24 * 60 * 60)
+        let activeKey1 = createFakeKeyDetails(expiration: now + 1 * 3600)
+        let activeKey2 = createFakeKeyDetails(expiration: now + 2 * 3600)
+        let activeKey3 = createFakeKeyDetails(expiration: now + 3 * 3600)
 
-        let nonExpiringKey = generateKey(expiration: nil)
-        let expiredKey = generateKey(expiration: now - 60 * 60)
-        let oldExpiredKey = generateKey(expiration: now - 24 * 60 * 60)
+        let nonExpiringKey = createFakeKeyDetails(expiration: nil)
+        let expiredKey = createFakeKeyDetails(expiration: now - 1 * 3600)
+        let oldExpiredKey = createFakeKeyDetails(expiration: now - 2 * 3600)
 
         let keyDetails = [revokedKey, oldExpiredKey, activeKey1, expiredKey, activeKey2, nonExpiringKey, activeKey3]
         let recipient = RecipientWithSortedPubKeys(email: "test@test.com",
                                                    keyDetails: keyDetails)
 
         XCTAssertEqual(recipient.pubKeys[0].fingerprint, nonExpiringKey.primaryFingerprint)
-        XCTAssertEqual(recipient.pubKeys[1].fingerprint, activeKey2.primaryFingerprint)
-        XCTAssertEqual(recipient.pubKeys[2].fingerprint, activeKey3.primaryFingerprint)
+        XCTAssertEqual(recipient.pubKeys[1].fingerprint, activeKey3.primaryFingerprint)
+        XCTAssertEqual(recipient.pubKeys[2].fingerprint, activeKey2.primaryFingerprint)
         XCTAssertEqual(recipient.pubKeys[3].fingerprint, activeKey1.primaryFingerprint)
         XCTAssertEqual(recipient.pubKeys[4].fingerprint, expiredKey.primaryFingerprint)
         XCTAssertEqual(recipient.pubKeys[5].fingerprint, oldExpiredKey.primaryFingerprint)
@@ -70,14 +70,14 @@ class RecipientTests: XCTestCase {
 }
 
 extension RecipientTests {
-    private func generateKey(expiration: Int?, revoked: Bool = false) -> KeyDetails {
+    private func createFakeKeyDetails(expiration: Int?, revoked: Bool = false) -> KeyDetails {
         KeyDetails(
             public: "Public part",
             private: nil,
             isFullyDecrypted: false,
             isFullyEncrypted: false,
-            ids: [KeyId(longid: randomString(length: 16),
-                        fingerprint: randomString(length: 16))],
+            ids: [KeyId(longid: randomString(length: 40),
+                        fingerprint: randomString(length: 40))],
             created: 1,
             lastModified: nil,
             expiration: expiration,
