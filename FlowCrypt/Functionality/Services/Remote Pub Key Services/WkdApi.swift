959e716833780a4d56508e4b57b3d98e5aad9d05
Ivan <ivan.ushakov@gmail.com>
2021-10-30 14:18:40 +0300

    issue #835 API error responses - code improvements (#855)
    * API error responses - code improvements
    
    * API error responses - code improvements

--- FlowCrypt/Functionality/Services/Remote Pub Key Services/WkdApi.swift
+++ FlowCrypt/Functionality/Services/Remote Pub Key Services/WkdApi.swift
@@ -20,7 +20,7 @@ class WkdApi: WkdApiType {
 
     private enum Constants {
         static let lookupEmailRequestTimeout: TimeInterval = 4
-        static let endpointName = "WkdApi"
+        static let apiName = "WkdApi"
     }
 
     private let urlConstructor: WkdUrlConstructorType
@@ -73,24 +73,24 @@ extension WkdApi {
 
     private func urlLookup(_ urls: WkdUrls) async throws -> (hasPolicy: Bool, key: Data?) {
         do {
-            let endpoint = ApiCall.Endpoint(
-                name: Constants.endpointName,
+            let request = ApiCall.Request(
+                apiName: Constants.apiName,
                 url: urls.policy,
                 timeout: Constants.lookupEmailRequestTimeout
             )
-            _ = try await ApiCall.asyncCall(endpoint)
+            _ = try await ApiCall.asyncCall(request)
         } catch {
             Logger.nested("WkdApi").logInfo("Failed to load \(urls.policy) with error \(error)")
             return (hasPolicy: false, key: nil)
         }
 
-        let endpoint = ApiCall.Endpoint(
-            name: Constants.endpointName,
+        let request = ApiCall.Request(
+            apiName: Constants.apiName,
             url: urls.pubKeys,
             timeout: Constants.lookupEmailRequestTimeout,
             tolerateStatus: [404]
         )
-        let pubKeyResponse = try await ApiCall.asyncCall(endpoint)
+        let pubKeyResponse = try await ApiCall.asyncCall(request)
         if !pubKeyResponse.data.toStr().isEmpty {
             Logger.nested("WKDURLsService").logInfo("Loaded WKD url \(urls.pubKeys) and will try to extract Public Keys")
         }
