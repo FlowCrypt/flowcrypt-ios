f061845824042db153f8196d5aac6dec8126c075
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2021-11-04 23:59:11 +0200

    issue #918 fix error message on Compose screen

---
+++ FlowCrypt/Extensions/Error+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Error+Extension.swift
+//  FlowCrypt
+//
+//  Created by Roma Sosnovsky on 04/11/21
+//  Copyright © 2017-present FlowCrypt a. s. All rights reserved.
+//
+    
+
+import Foundation
+
+extension Error {
+    var errorMessage: String {
+        switch self {
+        case let self as CustomStringConvertible:
+            return self.description
+        default:
+            return localizedDescription
+        }
+    }
+}
