8911fb3811af9b26ff1785b8f2fe5fac8ab9765d
Limon Monte <limon.monte@gmail.com>
2021-11-05 19:06:50 +0200

    move SVG icons to asset-sources/icons

--- Scripts/generate-icons.sh
+++ Scripts/generate-icons.sh
@@ -7,10 +7,10 @@ sizes=(20 29 40 50 57 58 60 72 76 80 87 100 114 120 144 152 167 180 1024)
 for size in "${sizes[@]}"
 do
   # Default
-  rsvg-convert -w $size -h $size --background-color white ./Scripts/flowcrypt-ios-svg.svg > ./FlowCrypt/Assets.xcassets/Appicon.appiconset/$size.png
+  rsvg-convert -w $size -h $size --background-color white ./asset-sources/icons/flowcrypt-ios-svg.svg > ./FlowCrypt/Assets.xcassets/Appicon.appiconset/$size.png
   # Enterprise
-  rsvg-convert -w $size -h $size --background-color white ./Scripts/flowcrypt-ios-enterprise-svg.svg > ./FlowCrypt/Assets.xcassets/Appicon-Enterprise.appiconset/$size.png
+  rsvg-convert -w $size -h $size --background-color white ./asset-sources/icons/flowcrypt-ios-enterprise-svg.svg > ./FlowCrypt/Assets.xcassets/Appicon-Enterprise.appiconset/$size.png
   # Debug
-  rsvg-convert -w $size -h $size --background-color white ./Scripts/flowcrypt-ios-debug-svg.svg > ./FlowCrypt/Assets.xcassets/Appicon-Debug.appiconset/$size.png
+  rsvg-convert -w $size -h $size --background-color white ./asset-sources/icons/flowcrypt-ios-debug-svg.svg > ./FlowCrypt/Assets.xcassets/Appicon-Debug.appiconset/$size.png
 done
 
