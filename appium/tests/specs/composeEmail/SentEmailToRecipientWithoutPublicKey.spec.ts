a8ec89a9da00810e2f2d3e27164a69ba5873d169
Dmitry Sotnikov <dmytro.s@accel-labs.com>
2021-11-15 15:25:52 +0200

    added test for sent email to recipient without public key

---
+++ appium/tests/specs/composeEmail/SentEmailToRecipientWithoutPublicKey.spec.ts
@@ -0,0 +1,34 @@
+import {
+  SplashScreen,
+  CreateKeyScreen,
+  InboxScreen,
+  NewMessageScreen,
+} from '../../screenobjects/all-screens';
+
+import { CommonData } from '../../data';
+import EmailScreen from "../../screenobjects/email.screen";
+
+describe('COMPOSE EMAIL: ', () => {
+
+  it('user is able to select recipient from contact list using contact name', async () => {
+
+    const noPublicKeyRecipient = "no.publickey@flowcrypt.com";
+    const emailSubject = CommonData.simpleEmail.subject;
+    const emailText = CommonData.simpleEmail.message;
+    const passPhrase = CommonData.account.passPhrase;
+    const noPublicKeyError = CommonData.errors.noPublicKey;
+
+    await SplashScreen.login();
+    await CreateKeyScreen.setPassPhrase();
+
+    await InboxScreen.clickCreateEmail();
+    await NewMessageScreen.composeEmail(noPublicKeyRecipient, emailSubject, emailText);
+    await NewMessageScreen.checkFilledComposeEmailInfo(noPublicKeyRecipient, emailSubject, emailText);
+    await NewMessageScreen.clickSentButton();
+    await EmailScreen.enterPassPhrase(passPhrase);
+
+    await EmailScreen.clickOkButton();
+    await NewMessageScreen.checkError(noPublicKeyError);
+
+  });
+});
