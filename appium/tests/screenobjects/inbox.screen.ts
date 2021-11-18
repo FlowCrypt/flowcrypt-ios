75d55e4a96b0f972c2ea3caa266c2b27e3af3b99
fcvakintos <91205853+fcvakintos@users.noreply.github.com>
2021-11-18 14:40:55 +0200

    issue #861 Added new spec for checking sent message after wrong pass phrase (#1027)
    * added new spec for checking sent message after setting incorrect pass phrase
    
    * added fixes after review
    
    * added fixes for Send Encrypted Email spec
    
    Co-authored-by: Dmitry Sotnikov <dmytro.s@accel-labs.com>

--- appium/tests/screenobjects/inbox.screen.ts
+++ appium/tests/screenobjects/inbox.screen.ts
@@ -6,6 +6,7 @@ const SELECTORS = {
   OK_BUTTON: '~Ok',
   CONFIRM_PASS_PHRASE_FIELD: '~textField',
   CREATE_EMAIL_BUTTON: '-ios class chain:**/XCUIElementTypeButton[`label == "+"`]',
+  INBOX_HEADER: '-ios class chain:**/XCUIElementTypeStaticText[`label == "INBOX"`]'
 };
 
 class InboxScreen extends BaseScreen {
@@ -17,18 +18,26 @@ class InboxScreen extends BaseScreen {
     return $(SELECTORS.CREATE_EMAIL_BUTTON);
   }
 
+  get inboxHeader() {
+      return $(SELECTORS.INBOX_HEADER)
+  }
+
   clickOnUserEmail = async (email: string) => {
     await (await this.createEmailButton).waitForDisplayed();
     await $(`~${email}`).click();
   }
 
   clickOnEmailBySubject = async (subject: string) => {
-    await (await this.createEmailButton).waitForDisplayed();
     await ElementHelper.waitAndClick(await $(`~${subject}`), 500);
   }
 
   clickCreateEmail = async () => {
     await ElementHelper.waitAndClick(await this.createEmailButton);
+  }
+
+  checkInboxScreen = async () => {
+    await (await this.inboxHeader).waitForDisplayed();
+    await (await this.createEmailButton).waitForDisplayed();
   }
 }
 
