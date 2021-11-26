bba5f0bacce50198969ab8ab354485377fc94a03
Ivan <ivan.ushakov@gmail.com>
2021-11-26 21:30:35 +0300

    All cases now

--- appium/tests/specs/inbox/CheckAllEmailSignatureCases.ts
+++ appium/tests/specs/inbox/CheckAllEmailSignatureCases.ts
@@ -1,7 +1,7 @@
 import {
   SplashScreen,
   SetupKeyScreen,
-  InboxScreen,
+  MailFolderScreen,
   EmailScreen
 } from '../../screenobjects/all-screens';
 
@@ -11,43 +11,44 @@ describe('INBOX: ', () => {
 
     await SplashScreen.login();
     await SetupKeyScreen.setPassPhrase();
-    await InboxScreen.checkInboxScreen();
+    await MailFolderScreen.checkInboxScreen();
 
     // signed+encrypted message
-    const s1 = 'Signed and encrypted message';
-    await InboxScreen.clickOnEmailBySubject(s1);
-    await EmailScreen.checkOpenedEmail('e2e.enterprise.test@flowcrypt.com', s1, 'Signed and encrypted message.');
+    await MailFolderScreen.clickOnEmailBySubject('Signed and encrypted message');
     await EmailScreen.checkEncryptionBadge('encrypted');
     await EmailScreen.checkSignatureBadge('signed');
     await EmailScreen.clickBackButton();
 
     // singed only message
-    const s2 = 'Signed only message';
-    await InboxScreen.clickOnEmailBySubject(s2);
-    await EmailScreen.checkOpenedEmail('e2e.enterprise.test@flowcrypt.com', s2, s2);
+    await MailFolderScreen.clickOnEmailBySubject('Signed only message');
     await EmailScreen.checkEncryptionBadge('not encrypted');
     await EmailScreen.checkSignatureBadge('signed');
     await EmailScreen.clickBackButton();
 
     // signed only message with detached signature
-    const s3 = 'Signed only message with detached signature';
-    await InboxScreen.clickOnEmailBySubject(s3);
-    await EmailScreen.checkOpenedEmail('e2e.enterprise.test@flowcrypt.com', s3, s3);
+    await MailFolderScreen.clickOnEmailBySubject('Signed only message with detached signature');
     await EmailScreen.checkEncryptionBadge('not encrypted');
     await EmailScreen.checkSignatureBadge('signed');
     await EmailScreen.clickBackButton();
 
     // plain message
-    const s4 = 'Test 1';
-    await InboxScreen.scrollDown();
-    await InboxScreen.clickOnEmailBySubject(s4);
-    await EmailScreen.checkOpenedEmail('dmitry@flowcrypt.com', s4, 'Test email');
+    await MailFolderScreen.scrollDown();
+    await MailFolderScreen.clickOnEmailBySubject('Test 1');
     await EmailScreen.checkEncryptionBadge('not encrypted');
-    await EmailScreen.checkSignatureBadge('not signed');    
+    await EmailScreen.checkSignatureBadge('not signed');
     await EmailScreen.clickBackButton();
 
-    // TODO signed-only message where the pubkey is not available
+    await MailFolderScreen.refreshMailList();
 
-    // TODO signed-only message that was tempered during transit
+    // signed only message where the pubkey is not available
+    await MailFolderScreen.clickOnEmailBySubject('Signed only message where the pubkey is not available');
+    await EmailScreen.checkEncryptionBadge('decrypt error');
+    await EmailScreen.clickBackButton();
+
+    // signed only message that was tempered during transit
+    await MailFolderScreen.clickOnEmailBySubject('Signed only message that was tempered during transit');
+    await EmailScreen.checkEncryptionBadge('not encrypted');
+    await EmailScreen.checkSignatureBadge('bad signature');
+    await EmailScreen.clickBackButton();
   });
 });
