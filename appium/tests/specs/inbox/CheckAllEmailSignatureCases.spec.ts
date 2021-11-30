9fee97c6817ec4fdf9492adb381af464a8bac85c
Ivan <ivan.ushakov@gmail.com>
2021-11-30 20:43:40 +0300

    Message signed by unknown key should have a different badge

--- appium/tests/specs/inbox/CheckAllEmailSignatureCases.spec.ts
+++ appium/tests/specs/inbox/CheckAllEmailSignatureCases.spec.ts
@@ -59,7 +59,8 @@ describe('INBOX: ', () => {
     await MailFolderScreen.clickSearchButton();
     await SearchScreen.searchAndClickEmailBySubject('Signed only message where the pubkey is not available');
 
-    await EmailScreen.checkEncryptionBadge('decrypt error');
+    await EmailScreen.checkEncryptionBadge('not encrypted');
+    await EmailScreen.checkSignatureBadge('cannot verify signature: no public key A54D82BE1521D20E');
     await EmailScreen.clickBackButton();
 
     await SearchScreen.clickBackButton();
