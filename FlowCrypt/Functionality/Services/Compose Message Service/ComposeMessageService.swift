ec53b2d9b9b7026ce6d6252996937b385f532fc2
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2021-12-03 23:15:20 +0200

    Show sending progress on ui (#1126)
    * Show sending progress on ui
    
    * Rework ComposeMessageService to use callbacks for state updates
    
    * Update progress status during composing

--- FlowCrypt/Functionality/Services/Compose Message Service/ComposeMessageService.swift
+++ FlowCrypt/Functionality/Services/Compose Message Service/ComposeMessageService.swift
@@ -41,7 +41,7 @@ final class ComposeMessageService {
     private let contactsService: ContactsServiceType
     private let core: CoreComposeMessageType & KeyParser
     private let draftGateway: DraftGateway?
-    private let logger: Logger
+    private lazy var logger: Logger = Logger.nested(Self.self)
 
     init(
         clientConfiguration: ClientConfiguration,
@@ -62,6 +62,11 @@ final class ComposeMessageService {
         self.logger = Logger.nested(in: Self.self, with: "ComposeMessageService")
     }
 
+    private var onStateChanged: ((State) -> Void)?
+    func onStateChanged(_ completion: ((State) -> Void)?) {
+        self.onStateChanged = completion
+    }
+
     func validateAndProduceSendableMsg(
         input: ComposeMessageInput,
         contextToSend: ComposeMessageContext,
@@ -69,6 +74,8 @@ final class ComposeMessageService {
         includeAttachments: Bool = true,
         signingPrv: PrvKeyInfo?
     ) async throws -> SendableMsg {
+        onStateChanged?(.validatingMessage)
+
         let recipients = contextToSend.recipients
         guard recipients.isNotEmpty else {
             throw MessageValidationError.emptyRecipient
@@ -106,6 +113,7 @@ final class ComposeMessageService {
         let allRecipientPubs = try await getPubKeys(for: recipients)
         let replyToMimeMsg = input.replyToMime
             .flatMap { String(data: $0, encoding: .utf8) }
+
         return SendableMsg(
             text: text,
             to: recipients.map(\.email),
@@ -162,21 +170,60 @@ final class ComposeMessageService {
     }
 
     // MARK: - Encrypt and Send
-    func encryptAndSend(message: SendableMsg, threadId: String?, progressHandler: ((Float) -> Void)?) async throws {
+    func encryptAndSend(message: SendableMsg, threadId: String?) async throws {
         do {
+            onStateChanged?(.startComposing)
             let r = try await core.composeEmail(
                 msg: message,
                 fmt: MsgFmt.encryptInline
             )
 
-            try await messageGateway.sendMail(input: MessageGatewayInput(mime: r.mimeEncoded, threadId: threadId),
-                                              progressHandler: progressHandler)
+            try await messageGateway.sendMail(
+                input: MessageGatewayInput(mime: r.mimeEncoded, threadId: threadId),
+                progressHandler: { [weak self] progress in
+                    self?.onStateChanged?(.progressChanged(progress))
+                }
+            )
+
             // cleaning any draft saved/created/fetched during editing
             if let draftId = draft?.identifier {
                 await draftGateway?.deleteDraft(with: draftId)
             }
+            onStateChanged?(.messageSent)
         } catch {
             throw ComposeMessageError.gatewayError(error)
+        }
+    }
+}
+
+extension ComposeMessageService {
+    enum State {
+        case idle
+        case validatingMessage
+        case startComposing
+        case progressChanged(Float)
+        case messageSent
+
+        var message: String? {
+            switch self {
+            case .idle:
+                return nil
+            case .validatingMessage:
+                return "Validating"
+            case .startComposing:
+                return "Encrypting"
+            case .progressChanged:
+                return "Uploading"
+            case .messageSent:
+                return "Message sent"
+            }
+        }
+
+        var progress: Float? {
+            guard case .progressChanged(let progress) = self else {
+                return nil
+            }
+            return progress
         }
     }
 }
