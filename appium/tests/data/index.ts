8a7015f660979b896ddf673c2bd7ebf0c155b4d9
fcvakintos <91205853+fcvakintos@users.noreply.github.com>
2021-12-03 15:58:51 +0200

    issue #1095 #1144 added tests for view message processing errors (#1154)
    Co-authored-by: Dmitry Sotnikov <dmytro.s@accel-labs.com>

--- appium/tests/data/index.ts
+++ appium/tests/data/index.ts
@@ -38,6 +38,28 @@ export const CommonData = {
     subject: 'Encrypted message with key mismatch',
     message: 'Could not decrypt:',
   },
+  encryptedMDCHashMismatchEmail: {
+    senderEmail: 'flowcrypt.compatibility@gmail.com',
+    subject: 'encrypted - MDC hash mismatch - modification detected - should fail',
+    message: 'bad_mdc: Security threat - opening this message is dangerous because it was modified in transit.',
+  },
+  encryptedForAnotherPublicKeyEmail: {
+    subject: 'message encrypted for another public key (only one pubkey used)',
+    message: 'key_mismatch: Missing appropriate key',
+    senderEmail: 'flowcrypt.compatibility@gmail.com',
+  },
+  wrongChecksumEmail: {
+    subject: 'wrong checksum',
+    message: 'format: Error: Ascii armor integrity check on message failed: \'FdCC\' should be \'FddK\'',
+    senderEmail: 'flowcrypt.compatibility@gmail.com',
+  },
+  notIntegrityProtected: {
+    subject: 'not integrity protected - should show a warning and not decrypt automatically',
+    message: '',
+    senderEmail: 'flowcrypt.compatibility@gmail.com',
+    encryptionBadgeText: 'not encrypted',
+    signatureBadgeText: 'not signed'
+  },
   recipientWithoutPublicKey: {
     email: 'no.publickey@flowcrypt.com'
   },
@@ -55,5 +77,8 @@ export const CommonData = {
       ' Please ask them to send you updated public key. If this is an enterprise installation, please ask your systems admin.',
     revokedPublicKey: 'Could not compose message One or more of your recipients have revoked public keys (marked in red).' +
       ' Please ask them to send you a new public key. If this is an enterprise installation, please ask your systems admin.'
+  },
+  decryptErrorBadge: {
+    badgeText: 'decrypt error'
   }
 };
