81828884084ed975baa6cbd2b89db0723c153b31
Ivan <ivan.ushakov@gmail.com>
2021-12-03 15:56:20 +0300

    issue #1114 Use Realm queries, don't re-implement database code (#1133)
    * Use Realm queries, don't re-implement database code
    
    * Code review fixes
    
    * Code review fixes

---
+++ FlowCryptAppTests/Mocks/EncryptedStorageMock.swift
@@ -0,0 +1,68 @@
+//
+//  EncryptedStorageMock.swift
+//  FlowCryptAppTests
+//
+//  Created by  Ivan Ushakov on 01.12.2021
+//  Copyright © 2017-present FlowCrypt a. s. All rights reserved.
+//
+
+@testable import FlowCrypt
+import RealmSwift
+
+final class EncryptedStorageMock: EncryptedStorageType {
+
+    var storage: Realm {
+        fatalError()
+    }
+
+    var activeUser: FlowCrypt.User?
+
+    var getAllUsersResult: [FlowCrypt.User] = []
+    func getAllUsers() -> [FlowCrypt.User] {
+        getAllUsersResult
+    }
+
+    func saveActiveUser(with user: FlowCrypt.User) {
+    }
+
+    var doesAnyKeypairExistResult = false
+    func doesAnyKeypairExist(for email: String) -> Bool {
+        doesAnyKeypairExistResult
+    }
+
+    func putKeypairs(keyDetails: [KeyDetails], passPhrase: String?, source: KeySource, for email: String) {
+    }
+
+    var getKeypairsResult: [KeyInfo] = []
+    func getKeypairs(by email: String) -> [KeyInfo] {
+        getKeypairsResult
+    }
+
+    func validate() throws {
+    }
+
+    func reset() throws {
+    }
+
+    func cleanup() {
+    }
+}
+
+extension EncryptedStorageMock {
+    static func createFakeKeyDetails(pub: String = "pubKey", expiration: Int?, revoked: Bool = false) -> KeyDetails {
+        KeyDetails(
+            public: pub,
+            private: nil,
+            isFullyDecrypted: false,
+            isFullyEncrypted: false,
+            ids: [KeyId(longid: String.random(length: 40),
+                        fingerprint: String.random(length: 40))],
+            created: 1,
+            lastModified: nil,
+            expiration: expiration,
+            users: ["Test User <test@flowcrypt.com>"],
+            algo: nil,
+            revoked: revoked
+        )
+    }
+}
