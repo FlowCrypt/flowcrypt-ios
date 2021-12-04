325da5032215faf04cc42f9bb55d20d1ed3d29ce
tomholub <info@nvimp.com>
2021-12-04 11:15:48 +0100

    remove dup test

--- appium/tests/specs/mock/setup/SetupFailsWithInconsistentClientConfiguration.spec copy.ts
+++ appium/tests/specs/mock/setup/SetupFailsWithInconsistentClientConfiguration.spec copy.ts
@@ -17,16 +17,4 @@ describe('SETUP: ', () => {
     });
   });
 
-  it('setup shows meaningful error when FES returns 400', async () => {
-    const mockApi = new MockApi();
-    mockApi.fesConfig = { returnError: { code: 400, message: "some client err" } };
-    await mockApi.withMockedApis(async () => {
-      await SplashScreen.login();
-      // todo - replace the following pause with wait for modal error
-      await browser.pause(5000);
-    });
-  });
-
-  // todo - app shows meaningful error when FES returns wrong err format
-
 });
