944c8e1665732f1dd5a5916e5b8fec64815ae9bf
Dmitry Sotnikov <dmytro.s@accel-labs.com>
2021-12-09 10:07:46 +0200

    added new specs for checking recipient after mock

---
+++ appium/tests/specs/mock/setup/CannotFindEmailOnAttester.spec.ts
@@ -0,0 +1,29 @@
+import { MockApi } from 'api-mocks/mock';
+import {
+    SplashScreen,
+} from '../../../screenobjects/all-screens';
+import {attesterPublicKeySamples} from "../../../../api-mocks/apis/attester/attester-endpoints";
+import SetupKeyScreen from "../../../screenobjects/setup-key.screen";
+import MailFolderScreen from "../../../screenobjects/mail-folder.screen";
+import NewMessageScreen from "../../../screenobjects/new-message.screen";
+
+describe('SETUP: ', () => {
+
+    it('Cannot find email on attester', async () => {
+
+        const mockApi = new MockApi();
+
+        mockApi.fesConfig = { clientConfiguration: {flags: ["NO_PRV_CREATE", "NO_PRV_BACKUP", "NO_ATTESTER_SUBMIT",
+                "PRV_AUTOIMPORT_OR_AUTOGEN", "FORBID_STORING_PASS_PHRASE"],
+                key_manager_url: "https://ekm.flowcrypt.com", disallow_attester_search_for_domains: ["*"]}};
+        mockApi.attesterConfig = {servedPubkeys: {'available.on@attester.test': attesterPublicKeySamples.valid}};
+
+        await mockApi.withMockedApis(async () => {
+            await SplashScreen.login();
+            await SetupKeyScreen.setPassPhrase();
+            await MailFolderScreen.checkInboxScreen();
+            await MailFolderScreen.clickCreateEmail();
+            await NewMessageScreen.setAddRecipient('available.on@attester.test');
+        });
+    });
+});
