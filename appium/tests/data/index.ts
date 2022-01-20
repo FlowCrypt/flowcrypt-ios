c660856eae629332be22fe527b937e3c85d231bf
Dmitry Sotnikov <dmytro.s@accel-labs.com>
2022-01-20 11:31:45 +0200

    added fixes after testrun

--- appium/tests/data/index.ts
+++ appium/tests/data/index.ts
@@ -55,8 +55,8 @@ export const CommonData = {
   },
   updateUser: {
     email: 'updating.key@example.test',
-    oldSignatureDate: 'Jan 12, 2022 at 9:16:58 PM',
-    newSignatureDate:  'Jan 12, 2022 at 9:27:20 PM',
+    oldSignatureDate: 'Jan 12, 2022 at 11:16:58 AM',
+    newSignatureDate:  'Jan 12, 2022 at 11:27:20 AM',
     oldFingerprints: '7E181662A26EC6748D6FDB1CC7C990C1A2EA78DD, 5C23518345A6595E81EBFEFCA71D94A76202B1D0',
     newFingerprints: '7E181662A26EC6748D6FDB1CC7C990C1A2EA78DD, 5C23518345A6595E81EBFEFCA71D94A76202B1D0, 40E4EE0325E38F717737889AC54F277266650211'
   },
