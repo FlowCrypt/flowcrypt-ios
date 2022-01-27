f75c075b80a5bf32759c86b3ea8ad2170bf1542c
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-01-27 13:24:47 +0200

    update composeEmail test helper

--- appium/tests/screenobjects/new-message.screen.ts
+++ appium/tests/screenobjects/new-message.screen.ts
@@ -116,8 +116,8 @@ class NewMessageScreen extends BaseScreen {
 
   composeEmail = async (recipient: string, subject: string, message: string) => {
     await this.setAddRecipient(recipient);
-    await this.setSubject(subject);
     await this.setComposeSecurityMessage(message);
+    await this.setSubject(subject);
   };
 
   setAddRecipientByName = async (name: string, email: string) => {
