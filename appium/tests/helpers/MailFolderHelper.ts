08159d5c7f1c006f1986ed566d30e8c0cf399053
Dmitry Sotnikov <dmytro.s@accel-labs.com>
2022-02-07 14:03:58 +0200

    added new spec for checking inbox pagination after loading all messages

--- appium/tests/helpers/MailFolderHelper.ts
+++ appium/tests/helpers/MailFolderHelper.ts
@@ -4,11 +4,11 @@ import MailFolderScreen from "../screenobjects/mail-folder.screen";
 class MailFolderHelper {
 
   static checkPagination = async (subject: string) => {
-    let emailsCountBeforeScroll = await MailFolderScreen.getEmailCount();
+    const emailsCountBeforeScroll = await MailFolderScreen.getEmailCount();
     await MailFolderScreen.scrollDownToEmail(subject);
     expect(emailsCountBeforeScroll).toEqual(40); //Should be changed to 20 when https://github.com/FlowCrypt/flowcrypt-ios/issues/1366 is fixed
 
-    let emailsCountAfterScroll = await MailFolderScreen.getEmailCount();
+    const emailsCountAfterScroll = await MailFolderScreen.getEmailCount();
     expect(emailsCountBeforeScroll).toBeLessThan(emailsCountAfterScroll);
   }
 }
