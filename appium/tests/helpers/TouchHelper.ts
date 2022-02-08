626764afecfd2b325b2bd920c2dcc2d8e6600d99
fcvakintos <91205853+fcvakintos@users.noreply.github.com>
2022-02-08 11:26:52 +0200

    added new spec for checking inbox pagination after loading all messages (#1368)

--- appium/tests/helpers/TouchHelper.ts
+++ appium/tests/helpers/TouchHelper.ts
@@ -53,5 +53,28 @@ class TouchHelper {
     }
     throw new Error(`Element ${JSON.stringify(element.selector)} not displayed after scroll`);
   }
+
+  static scrollUpToElement = async (element: WebdriverIO.Element) => {
+    const { width, height } = await driver.getWindowSize();
+    const anchor = width / 2;
+    const startPoint = height * 0.15;
+    const endPoint = height * 0.75;
+
+    // this wait can be later replaced by waiting for loader to go away before scrolling
+    await browser.pause(1000); // make sure contents are loaded first, so we don't scroll too early
+    for(let i = 0; i < 15; i++) {
+      if (await element.isDisplayed()) {
+       return;
+      }
+      await driver.touchPerform([
+        {action: 'press', options: {x: anchor, y: startPoint}},
+        {action: 'wait', options: {ms: 100}},
+        {action: 'moveTo', options: {x: anchor, y: endPoint}},
+        {action: 'release', options: {}},
+      ]);
+      await browser.pause(1000); // due to scroll action which takes about second
+    }
+    throw new Error(`Element ${JSON.stringify(element.selector)} not displayed after scroll`);
+  }
 }
 export default TouchHelper;
