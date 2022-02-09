0c68330ceb59cf9cf07f283b3d2b8cdc70251cc1
Ivan Pizhenko <Ivan.Pizhenko@users.noreply.github.com>
2022-02-09 06:51:12 +0200

    wip

--- Core/tooling/build.sh
+++ Core/tooling/build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # fix openpgp node_modules
 cp -f source/core/types/openpgp.d.ts node_modules/openpgp
-cp -f source/lib/openpgp.js node_modules/dist/node
+cp -f source/lib/openpgp.js node_modules/openpgp/dist/node
 
 # clean up
 rm -rf build/ts build/bundles build/final/*
