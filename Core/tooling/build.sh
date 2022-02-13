4896c9604d1598b0d8572ebfa63814d6a7dadcbe
Ivan Pizhenko <Ivan.Pizhenko@users.noreply.github.com>
2022-02-13 02:23:21 +0200

    patch web-stream-tools

--- Core/tooling/build.sh
+++ Core/tooling/build.sh
@@ -2,11 +2,27 @@
 
 set -euxo pipefail
 
-# fix openpgp in node_modules
-echo "Patching openpgp.js v5..."
 # will remove this when openpgp.js will release all our fixes
+echo "Patching openpgp.js v5..."
 cp -fv ./source/core/types/openpgp.d.ts ./node_modules/openpgp/
 echo "Patching openpgp.js v5 - DONE."
+
+# will remove this when @openpgp/web-stream-tools would release their type definitions
+echo "Patching @openpgp/web-stream-tools ..."
+cp -fv ./source/core/types/web-stream-tools.d.ts ./node_modules/@openpgp/web-stream-tools/
+wst_pkg=./node_modules/@openpgp/web-stream-tools/package.json
+set +e
+wst_patched=$(grep -n '"types":' $wst_pkg | wc -l)
+set -e
+if [ $wst_patched = 0 ]; then
+    wst_pkg_tmp=${wst_pkg}.tmp
+    n=$(grep -n '"main":' $wst_pkg | cut -f1 -d':')
+    head -$n ${wst_pkg} >${wst_pkg_tmp}
+    echo '  "types": "web-stream-tools.d.ts",' >>${wst_pkg_tmp}
+    tail -n $((n+1)) ${wst_pkg} >>${wst_pkg_tmp}
+    mv -f ${wst_pkg_tmp} ${wst_pkg}
+fi
+echo "Patching @openpgp/web-stream-tools - DONE."
 
 # clean up
 rm -rf ./build/ts ./build/bundles ./build/final/*
