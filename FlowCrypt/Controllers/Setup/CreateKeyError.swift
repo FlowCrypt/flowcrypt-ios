c18b97049cc40e1ef9da1d2208c09d0a840f794e
Anton Kharchevskyi <kharchevskyi.a@gmail.com>
2022-02-23 21:07:12 +0000

    Add uncommited file

---
+++ FlowCrypt/Controllers/Setup/CreateKeyError.swift
@@ -0,0 +1,49 @@
+//
+//  CreateKeyError.swift
+//  FlowCrypt
+//
+//  Created by Anton Kharchevskyi on 27.11.2020.
+//  Copyright © 2017-present FlowCrypt a. s. All rights reserved.
+//
+
+import UIKit
+
+enum CreateKeyError: Error {
+    case weakPassPhrase(_ strength: CoreRes.ZxcvbnStrengthBar)
+    /// Missing user email
+    case missingUserEmail
+    /// Missing user name
+    case missingUserName
+    /// Pass phrases don't match
+    case doesntMatch
+    /// Silent abort
+    case conformingPassPhraseError
+    /// Failed to submit key
+    case submitKey(Error)
+}
+
+extension CreateKeyError: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .weakPassPhrase(let strength):
+            return """
+            Pass phrase strength: \(strength.word.word)
+            crack time: \(strength.time)
+
+            We recommend to use 5-6 unrelated words as your Pass Phrase.
+            """
+        case .missingUserEmail:
+            return "backupServiceError_email".localized
+        case .missingUserName:
+            return "backupServiceError_name".localized
+        case .doesntMatch:
+            return "pass_phrase_match_error".localized
+        case .submitKey(let error):
+            return "submit_key_error".localized
+                + "\n"
+                + "\(error.errorMessage)"
+        case .conformingPassPhraseError:
+            return ""
+        }
+    }
+}
