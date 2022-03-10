008a133c5b734dbf2e1fd3f9a786c325db149260
Ioan Moldovan <ioanmo226@gmail.com>
2022-03-10 05:45:25 -0400

    #1408 Appium UI test for respecting cc on reply all

--- appium/tests/specs/live/inbox/CheckReplyAndForwardForEncryptedEmail.spec.ts
+++ appium/tests/specs/live/inbox/CheckReplyAndForwardForEncryptedEmail.spec.ts
@@ -13,11 +13,12 @@ describe('INBOX: ', () => {
 
   it('user is able to reply or forward email and check info from composed email', async () => {
 
-    const senderEmail = CommonData.emailWithMultipleRecipients.sender;
-    const recipientEmail = CommonData.emailWithMultipleRecipients.recipient;
-    const emailSubject = CommonData.emailWithMultipleRecipients.subject;
-    const emailText = CommonData.emailWithMultipleRecipients.message;
-    const encryptedAttachmentName = CommonData.emailWithMultipleRecipients.encryptedAttachmentName;
+    const senderEmail = CommonData.emailWithMultipleRecipientsWithCC.sender;
+    const recipientEmail = CommonData.emailWithMultipleRecipientsWithCC.recipient;
+    const ccEmail = CommonData.emailWithMultipleRecipientsWithCC.cc;
+    const emailSubject = CommonData.emailWithMultipleRecipientsWithCC.subject;
+    const emailText = CommonData.emailWithMultipleRecipientsWithCC.message;
+    const encryptedAttachmentName = CommonData.emailWithMultipleRecipientsWithCC.encryptedAttachmentName;
 
     const replySubject = `Re: ${emailSubject}`;
     const forwardSubject = `Fwd: ${emailSubject}`;
@@ -39,7 +40,7 @@ describe('INBOX: ', () => {
     // check reply all message
     await EmailScreen.clickMenuButton();
     await EmailScreen.clickReplyAllButton();
-    await NewMessageScreen.checkFilledComposeEmailInfo([recipientEmail, senderEmail], replySubject, quoteText);
+    await NewMessageScreen.checkFilledComposeEmailInfo([recipientEmail, senderEmail], replySubject, quoteText, undefined, [ccEmail]);
     await NewMessageScreen.clickBackButton();
 
     // check forwarded message
