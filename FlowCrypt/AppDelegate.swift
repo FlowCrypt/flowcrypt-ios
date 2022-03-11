29953aea8408838082a78699d56006868ea79d9e
Ioan Moldovan <ioanmo226@gmail.com>
2022-03-11 09:59:55 -0400

    #1373 fix realm decryption issue when prewarm mode

--- FlowCrypt/AppDelegate.swift
+++ FlowCrypt/AppDelegate.swift
@@ -7,15 +7,28 @@ import AppAuth
 import UIKit
 import GTMAppAuth
 import FlowCryptCommon
+import Combine
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate, AppDelegateGoogleSesssionContainer {
     var blurViewController: BlurViewController?
     var googleAuthSession: OIDExternalUserAgentSession?
     let window = UIWindow(frame: UIScreen.main.bounds)
+    private var cancellable = Set<AnyCancellable>()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if application.isRunningTests {
+            return true
+        }
+        // Avoid race condition where the app might try to access keychain data before the device has decrypted it
+        guard UIApplication.shared.isProtectedDataAvailable else {
+            NotificationCenter
+                 .default
+                 .publisher(for: UIApplication.protectedDataDidBecomeAvailableNotification)
+                 .first()
+                 .sink { _ in
+                     GlobalRouter().proceed()
+                 }.store(in: &cancellable)
             return true
         }
         GlobalRouter().proceed()
@@ -33,10 +46,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, AppDelegateGoogleSesssion
 
 extension AppDelegate: BlursTopView {
     func applicationWillResignActive(_ application: UIApplication) {
+        cancellable.forEach { $0.cancel() }
         if !isBlurViewShowing() {
             coverTopViewWithBlurView()
         }
     }
+
     func applicationDidBecomeActive(_ application: UIApplication) {
         if isBlurViewShowing() {
             removeBlurView()
