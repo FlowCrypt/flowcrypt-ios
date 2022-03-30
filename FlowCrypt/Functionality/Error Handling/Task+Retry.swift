e9767dc332b0dbf1852d6a347679910b8267ff1c
Ioan Moldovan <ioanmo226@gmail.com>
2022-03-30 11:01:30 -0400

    #1442 added task retry feature

---
+++ FlowCrypt/Functionality/Error Handling/Task+Retry.swift
@@ -0,0 +1,34 @@
+//
+//  Task+Retry.swift
+//  FlowCrypt
+//
+//  Created by Ioan Moldovan on 3/30/22
+//  Copyright © 2017-present FlowCrypt a. s. All rights reserved.
+//
+
+extension Task where Failure == Error {
+    @discardableResult
+    static func retrying(
+        priority: TaskPriority? = nil,
+        maxRetryCount: Int = 3,
+        retryDelay: TimeInterval = 1,
+        operation: @Sendable @escaping () async throws -> Success
+    ) -> Task {
+        Task(priority: priority) {
+            for _ in 0..<maxRetryCount {
+                do {
+                    return try await operation()
+                } catch {
+                    let oneSecond = TimeInterval(1_000_000_000)
+                    let delay = UInt64(oneSecond * retryDelay)
+                    try await Task<Never, Never>.sleep(nanoseconds: delay)
+
+                    continue
+                }
+            }
+
+            try Task<Never, Never>.checkCancellation()
+            return try await operation()
+        }
+    }
+}
