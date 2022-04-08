80612683c9914766b4acc13b7b72805a1cccf2cd
Ioan Moldovan <mobilestar108@outlook.com>
2022-04-08 11:03:39 -0400

    #1471 update iOS simulator version

--- appium/config/wdio.live.conf.js
+++ appium/config/wdio.live.conf.js
@@ -29,7 +29,7 @@ config.capabilities = [
     platformName: 'iOS',
     iosInstallPause: 5000,
     deviceName: 'iPhone 13',
-    platformVersion: '15.2',
+    platformVersion: '15.3',
     automationName: 'XCUITest',
     app: join(process.cwd(), './FlowCrypt.app'),
     simpleIsVisibleCheck: true,
