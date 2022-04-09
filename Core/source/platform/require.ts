a6d66cf082c5f075957a7b0c68fc9fd7273fd782
Ivan Pizhenko <Ivan.Pizhenko@users.noreply.github.com>
2022-04-10 01:28:06 +0300

    Remove duplicate declaration

--- Core/source/platform/require.ts
+++ Core/source/platform/require.ts
@@ -30,8 +30,6 @@ export interface NodeStream<T extends Uint8Array | string> extends BaseStream<T>
 
 export type MaybeStream<T extends Uint8Array | string> = T | WebStream<T> | NodeStream<T>;
 
-export type MaybeStream<T extends Uint8Array | string> = T | WebStream<T> | NodeStream<T>;
-
 type ReadToEndFn = <T extends Uint8Array | string>(input: MaybeStream<T>, concat?: (list: T[]) => T) => Promise<T>;
 
 export const requireStreamReadToEnd = (): ReadToEndFn => {
