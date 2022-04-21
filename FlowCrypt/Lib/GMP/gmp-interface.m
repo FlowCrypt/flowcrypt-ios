713a0e0c30ff0548fed0a899281e49b30998288f
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-04-21 18:17:24 +0300

    Allow c_gmp_mod_pow usage on all platforms

--- FlowCrypt/Lib/GMP/gmp-interface.m
+++ FlowCrypt/Lib/GMP/gmp-interface.m
@@ -2,7 +2,6 @@
 #include <stdio.h>
 
 const char* c_gmp_mod_pow(const char* base, const char* exponent, const char* modulo) {
-#ifdef __arm64__
     mpz_t mpz_base, mpz_exponent, mpz_modulo, mpz_result;
     mpz_inits (mpz_base, mpz_exponent, mpz_modulo, mpz_result, NULL);
     if (mpz_set_str (mpz_base, base, 10) != 0) {
@@ -20,8 +19,4 @@ const char* c_gmp_mod_pow(const char* base, const char* exponent, const char* mo
     // mpz_result = mpz_base ^ mpz_exponent mod mpz_modulo
     mpz_powm (mpz_result, mpz_base, mpz_exponent, mpz_modulo);
     return mpz_get_str (NULL, 10, mpz_result);
-#else
-    printf("c_gmp_mod_pow is not supported on this architecture\n");
-    return "";
-#endif
 }
