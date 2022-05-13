8d1e041872723730fe0c44b1805e2fc78fae57fa
fcvakintos <91205853+fcvakintos@users.noreply.github.com>
2022-05-14 01:02:53 +0300

    issue #1531 added new spec for checking recipients when replying to own message (#1582)
    * added new spec for checking recipient name when reply the message and changing recipient
    
    * added fixes after review
    
    * added changes after review
    
    * merged the last master version
    
    * removed todo comments from the test
    
    Co-authored-by: Dmitry Sotnikov <dmytro.s@accel-labs.com>
    Co-authored-by: tomholub <info@nvimp.com>

--- appium/tests/specs/live/inbox/CheckThreadRendering.spec.ts
+++ appium/tests/specs/live/inbox/CheckThreadRendering.spec.ts
@@ -24,8 +24,8 @@ describe('INBOX: ', () => {
     await MailFolderScreen.checkInboxScreen();
 
     await MailFolderScreen.clickOnEmailBySubject(emailSubject);
-    await EmailScreen.checkThreadMessage(senderName, emailSubject, thirdMessage, dateThird, 2);
-    await EmailScreen.checkThreadMessage(userEmail, emailSubject, secondMessage, dateSecond, 1);
-    await EmailScreen.checkThreadMessage(senderName, emailSubject, firstMessage, dateFirst);
+    await EmailScreen.checkThreadMessage(senderName, emailSubject, thirdMessage,2, dateThird);
+    await EmailScreen.checkThreadMessage(userEmail, emailSubject, secondMessage, 1, dateSecond);
+    await EmailScreen.checkThreadMessage(senderName, emailSubject, firstMessage, 0, dateFirst);
   });
 });
