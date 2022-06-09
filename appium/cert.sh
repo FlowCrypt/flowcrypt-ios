3661e06c59a33937824999aa4223c1b3a00945dd
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-06-09 16:08:22 +0300

    cert fix

---
+++ appium/cert.sh
@@ -0,0 +1,17 @@
+IOS_SIM_UDID=$(xcrun simctl list devices | grep "iPhone 13" | grep -E -o -i "([0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12})");
+SIMULATOR_PATH='/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator'
+
+open -a "$SIMULATOR_PATH" --args -CurrentDeviceUDID $IOS_SIM_UDID
+
+function booted_sim_ct() {
+  echo `xcrun simctl list | grep Booted | wc -l | sed -e 's/ //g'`
+}
+
+while [ `booted_sim_ct` -lt 1 ]
+do
+  sleep 1
+done
+
+sleep 10
+
+xcrun simctl keychain "iPhone 13" add-root-cert ./api-mocks/mock-ssl-cert/cert.pem
