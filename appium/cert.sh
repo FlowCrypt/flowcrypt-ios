451dc0aa96f348c59ac1ba27740e58e88d45b586
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-06-13 14:20:03 +0300

    update cert.sh

--- appium/cert.sh
+++ appium/cert.sh
@@ -1,11 +1,11 @@
-sleep 15
+#!/usr/bin/env bash
 
 IOS_SIM_UDID=$(xcrun simctl list devices available 15.5 | grep "iPhone 13 (" | grep -E -o -i "([0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12})")
 SIMULATOR_PATH='/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/Contents/MacOS/Simulator'
 
 echo "Found UUID of iPhone 13 - ${IOS_SIM_UDID}"
 
-open -a "$SIMULATOR_PATH" --args -CurrentDeviceUDID $IOS_SIM_UDID
+open -a $SIMULATOR_PATH --args -CurrentDeviceUDID $IOS_SIM_UDID
 
 function booted_sim_ct() {
   echo `xcrun simctl list devices 15.5 | grep Booted | wc -l | sed -e 's/ //g'`
@@ -20,7 +20,7 @@ sleep 20
 
 echo 'BOOTED'
 xcrun simctl list devices booted 15.5 
-# xcrun simctl keychain $IOS_SIM_UDID add-root-cert ./appium/api-mocks/mock-ssl-cert/cert.pem
+xcrun simctl keychain $IOS_SIM_UDID add-root-cert ./appium/api-mocks/mock-ssl-cert/cert.pem
 xcrun simctl list devices available 15.5
 
 sleep 5
