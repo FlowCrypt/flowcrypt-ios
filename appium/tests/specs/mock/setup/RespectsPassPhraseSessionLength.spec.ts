c67ed9e671651441ed5e01299586db0e59c0469f
Ioan Moldovan <mobilestar108@outlook.com>
2022-10-05 05:21:00 -0400

    fix: pr reviews

--- appium/tests/specs/mock/setup/RespectsPassPhraseSessionLength.spec.ts
+++ appium/tests/specs/mock/setup/RespectsPassPhraseSessionLength.spec.ts
@@ -1,9 +1,11 @@
 import { ekmKeySamples } from 'api-mocks/apis/ekm/ekm-endpoints';
 import { MockApi } from 'api-mocks/mock';
+import { MockApiConfig } from 'api-mocks/mock-config';
 import { CommonData } from 'tests/data';
 import AppiumHelper from 'tests/helpers/AppiumHelper';
 import BaseScreen from 'tests/screenobjects/base.screen';
 import {
+  EmailScreen,
   SplashScreen
 } from '../../../screenobjects/all-screens';
 import MailFolderScreen from "../../../screenobjects/mail-folder.screen";
@@ -17,12 +19,7 @@ describe('SETUP: ', () => {
     const testMessageSubject = 'Message with cc and multiple recipients and text attachment';
     const processArgs = CommonData.mockProcessArgs;
 
-    mockApi.fesConfig = {
-      clientConfiguration: {
-        flags: ["NO_PRV_CREATE", "NO_PRV_BACKUP", "NO_ATTESTER_SUBMIT", "PRV_AUTOIMPORT_OR_AUTOGEN", "FORBID_STORING_PASS_PHRASE"],
-        key_manager_url: CommonData.keyManagerURL.mockServer
-      }
-    };
+    mockApi.fesConfig = MockApiConfig.defaultEnterpriseFesConfiguration;
     mockApi.ekmConfig = {
       returnKeys: [ekmKeySamples.e2e.prv]
     }
@@ -43,12 +40,15 @@ describe('SETUP: ', () => {
       // stage 3: set in_memory_pass_phrase_session_length and check if passphrase expires in 5 seconds
       mockApi.fesConfig = {
         clientConfiguration: {
-          flags: ["NO_PRV_CREATE", "NO_PRV_BACKUP", "NO_ATTESTER_SUBMIT", "PRV_AUTOIMPORT_OR_AUTOGEN", "FORBID_STORING_PASS_PHRASE"],
-          key_manager_url: CommonData.keyManagerURL.mockServer,
+          ...MockApiConfig.defaultEnterpriseFesConfiguration.clientConfiguration,
           in_memory_pass_phrase_session_length: 5
         }
       };
       await AppiumHelper.restartApp(processArgs);
+      await MailFolderScreen.clickOnEmailBySubject(testMessageSubject);
+      await EmailScreen.enterPassPhrase(CommonData.account.passPhrase);
+      await EmailScreen.clickOkButton();
+      await EmailScreen.clickBackButton();
       await browser.pause(5000);
       await MailFolderScreen.clickOnEmailBySubject(testMessageSubject);
       await BaseScreen.checkModalMessage('Please enter pass phrase');
