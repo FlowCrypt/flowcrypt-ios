4bcbebf00d90fd9112259d4e11b671b2313c2408
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-10-25 23:04:19 +0300

    #1878 Fix sending email with invalid recipient (#1890)
    * improve message validation before sending
    
    * update ui test

--- appium/tests/specs/mock/composeEmail/CheckInvalidEmailRecipient.spec.ts
+++ appium/tests/specs/mock/composeEmail/CheckInvalidEmailRecipient.spec.ts
@@ -9,15 +9,26 @@ import { CommonData } from '../../../data';
 import BaseScreen from "../../../screenobjects/base.screen";
 import { MockApi } from 'api-mocks/mock';
 import { MockApiConfig } from 'api-mocks/mock-config';
+import { MockUserList } from 'api-mocks/mock-data';
 
 describe('COMPOSE EMAIL: ', () => {
 
   it('user should enter correct email address into recipients', async () => {
 
+    const invalidRecipient = 'abc';
+    const recipient = MockUserList.dmitry;
+    const subject = CommonData.simpleEmail.subject;
+    const message = CommonData.simpleEmail.message;
+
     const mockApi = new MockApi();
 
     mockApi.fesConfig = MockApiConfig.defaultEnterpriseFesConfiguration;
     mockApi.ekmConfig = MockApiConfig.defaultEnterpriseEkmConfiguration;
+    mockApi.attesterConfig = {
+      servedPubkeys: {
+        [recipient.email]: recipient.pub!
+      }
+    };
 
     const invalidRecipientError = CommonData.errors.invalidRecipient;
 
@@ -27,7 +38,17 @@ describe('COMPOSE EMAIL: ', () => {
       await MailFolderScreen.checkInboxScreen();
       await MailFolderScreen.clickCreateEmail();
 
-      await NewMessageScreen.setAddRecipient('abc');
+      await NewMessageScreen.setAddRecipient(invalidRecipient);
+      await BaseScreen.checkModalMessage(invalidRecipientError);
+      await BaseScreen.clickOkButtonOnError();
+
+      await NewMessageScreen.deleteEnteredRecipient(invalidRecipient);
+      await NewMessageScreen.composeEmail(recipient.email, subject, message);
+      await NewMessageScreen.setAddRecipient(invalidRecipient);
+      await BaseScreen.checkModalMessage(invalidRecipientError);
+      await BaseScreen.clickOkButtonOnError();
+
+      await NewMessageScreen.clickSendButton();
       await BaseScreen.checkModalMessage(invalidRecipientError);
       await BaseScreen.clickOkButtonOnError();
     });
