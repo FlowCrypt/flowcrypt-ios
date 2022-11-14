6b82d0f28695aa0ae7703624cbff04becfed4452
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2022-11-14 20:41:47 +0200

    test fix

--- appium/tests/screenobjects/new-message.screen.ts
+++ appium/tests/screenobjects/new-message.screen.ts
@@ -175,8 +175,8 @@ class NewMessageScreen extends BaseScreen {
   };
 
   setComposeSecurityMessage = async (message: string) => {
-    await browser.pause(500);
     await ElementHelper.waitClickAndType(await this.composeSecurityMessage, message);
+    await browser.pause(500);
   };
 
   filledSubject = async (subject: string) => {
