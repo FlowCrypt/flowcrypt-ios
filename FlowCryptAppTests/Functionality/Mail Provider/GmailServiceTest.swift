eb708062999456b394de77f2056bdfae220bc81b
Ioan Moldovan <ioanmo226@gmail.com>
2022-12-27 10:24:33 -0400

    fix: swift unit test

--- FlowCryptAppTests/Functionality/Mail Provider/GmailServiceTest.swift
+++ FlowCryptAppTests/Functionality/Mail Provider/GmailServiceTest.swift
@@ -44,7 +44,9 @@ class GmailServiceTest: XCTestCase {
 
 // MARK: - Mock
 class GoogleAuthManagerMock: GoogleAuthManagerType {
-    var authorization: GTMAppAuthFetcherAuthorization?
+    func authorizationFor(email: String?) -> GTMAppAuthFetcherAuthorization? {
+        return nil
+    }
 
     var isContactsScopeEnabled = true
     func searchContacts(query: String) async throws -> [Recipient] {
