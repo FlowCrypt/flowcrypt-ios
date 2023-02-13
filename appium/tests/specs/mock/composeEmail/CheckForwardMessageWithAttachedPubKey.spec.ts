9aadf5f3b8c00e948f8e4a0b99098b88d1ae7526
Ioan Moldovan <ioanmo226@gmail.com>
2023-02-13 12:43:13 -0400

    fix: forward message issue for email with attached pub key

---
+++ appium/tests/specs/mock/composeEmail/CheckForwardMessageWithAttachedPubKey.spec.ts
@@ -0,0 +1,34 @@
+import { MockApi } from 'api-mocks/mock';
+import { MockApiConfig } from 'api-mocks/mock-config';
+import {
+  EmailScreen,
+  MailFolderScreen,
+  NewMessageScreen,
+  SetupKeyScreen,
+  SplashScreen
+} from '../../../screenobjects/all-screens';
+
+describe('COMPOSE EMAIL: ', () => {
+
+  it('check forward message with attached pub key', async () => {
+    const mockApi = new MockApi();
+    const subject = 'Test forward message with attached pub key';
+
+    mockApi.fesConfig = MockApiConfig.defaultEnterpriseFesConfiguration;
+    mockApi.ekmConfig = MockApiConfig.defaultEnterpriseEkmConfiguration;
+    mockApi.addGoogleAccount('e2e.enterprise.test@flowcrypt.com', {
+      messages: [subject],
+    });
+
+    await mockApi.withMockedApis(async () => {
+      await SplashScreen.mockLogin();
+      await SetupKeyScreen.setPassPhrase();
+      await MailFolderScreen.checkInboxScreen();
+      await MailFolderScreen.clickOnEmailBySubject(subject);
+
+      await EmailScreen.clickMenuButton();
+      await EmailScreen.clickForwardButton();
+      await NewMessageScreen.checkSubject(`Fwd: ${subject}`);
+    });
+  });
+});
