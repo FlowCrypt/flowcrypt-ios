50e10d1c710695fad9312699eeef35ae6a9ba16e
Roma Sosnovsky <roma.sosnovsky@gmail.com>
2023-06-13 15:18:42 +0300

    fix

--- appium/config/wdio.mock.conf.ts
+++ appium/config/wdio.mock.conf.ts
@@ -1,41 +1,28 @@
-import { join } from 'path';
-import { config } from './wdio.shared.conf';
+import { join } from "path";
+import { config } from "./wdio.shared.conf";
 
 config.suites = {
-  all: [
-    '../tests/specs/mock/**/*.spec.ts'
-  ],
-  settings: [
-    '../tests/specs/mock/settings/*.spec.ts'
-  ],
-  inbox: [
-    '../tests/specs/mock/inbox/*.spec.ts'
-  ],
-  compose: [
-    '../tests/specs/mock/composeEmail/*.spec.ts'
-  ],
-  login: [
-    '../tests/specs/mock/login/*.spec.ts'
-  ],
-  setup: [
-    '../tests/specs/mock/setup/*.spec.ts'
-  ],
-  drafts: [
-    '../tests/specs/mock/composeEmail/CheckDraftsFunctionality.spec.ts'
-  ]
+  all: ["../tests/specs/mock/**/*.spec.ts"],
+  settings: ["../tests/specs/mock/settings/*.spec.ts"],
+  inbox: ["../tests/specs/mock/inbox/*.spec.ts"],
+  compose: ["../tests/specs/mock/composeEmail/*.spec.ts"],
+  login: ["../tests/specs/mock/login/*.spec.ts"],
+  setup: ["../tests/specs/mock/setup/*.spec.ts"],
+  drafts: ["../tests/specs/mock/composeEmail/CheckDraftsFunctionality.spec.ts"],
 };
-
 
 config.capabilities = [
   {
-    platformName: 'iOS',
-    hostname: '127.0.0.1',
-    'appium:automationName': 'XCUITest',
-    'appium:processArguments': { 'args': ['--mock-fes-api', '--mock-attester-api', '--mock-gmail-api'] },
-    'appium:options': {
-      deviceName: 'iPhone 14',
-      platformVersion: '16.2',
-      app: join(process.cwd(), './FlowCrypt.app'),
+    platformName: "iOS",
+    hostname: "127.0.0.1",
+    "appium:automationName": "XCUITest",
+    "appium:processArguments": {
+      args: ["--mock-fes-api", "--mock-attester-api", "--mock-gmail-api"],
+    },
+    "appium:options": {
+      deviceName: "iPhone 14",
+      platformVersion: "16.3",
+      app: join(process.cwd(), "./FlowCrypt.app"),
     },
   },
 ];
