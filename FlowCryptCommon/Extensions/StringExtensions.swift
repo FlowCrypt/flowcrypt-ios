e3b2ae78b70b74bd220edf2e584ccbe99e759353
Ioan Moldovan <ioanmo226@gmail.com>
2023-07-24 13:17:20 -0400

    fix: pr review

--- FlowCryptCommon/Extensions/StringExtensions.swift
+++ FlowCryptCommon/Extensions/StringExtensions.swift
@@ -96,7 +96,7 @@ public extension String {
     }
 
     func isHTMLString() -> Bool {
-        if let _ = self.range(of: "^\\s*(<!doctype html>)?\\s*<[a-z][\\s\\S]*>\\s*$", options: .regularExpression) {
+        if let _ = self.range(of: "^\\s*(<!doctype html[\\s\\S]*>)?\\s*<[a-z][\\s\\S]*>\\s*$", options: [.regularExpression, .caseInsensitive]) {
             return true
         }
         return false
