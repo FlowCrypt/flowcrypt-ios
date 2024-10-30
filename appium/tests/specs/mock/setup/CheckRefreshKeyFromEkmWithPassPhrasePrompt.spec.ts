0c884c5d2d877ba422247ca58ce665d63d719397
Ioan Moldovan <mobilestar108@outlook.com>
2024-10-30 03:00:59 -0500

    fix: try to enter passphrase

--- appium/tests/specs/mock/setup/CheckRefreshKeyFromEkmWithPassPhrasePrompt.spec.ts
+++ appium/tests/specs/mock/setup/CheckRefreshKeyFromEkmWithPassPhrasePrompt.spec.ts
@@ -37,6 +37,7 @@ describe('SETUP: ', () => {
       await RefreshKeyScreen.clickOkButton();
       await BaseScreen.checkModalMessage(CommonData.refreshingKeysFromEkm.wrongPassPhrase);
       await RefreshKeyScreen.clickSystemOkButton();
+      await RefreshKeyScreen.fillPassPhrase('wrong passphrase');
       await RefreshKeyScreen.cancelRefresh();
       await KeysScreen.openScreenFromSideMenu();
       await KeysScreen.checkKeysScreen([ekmKeySamples.key0]);
