6b1854812e7d1b2353441aaa8879096c7a7b2d92
Ioan Moldovan <mobilestar108@outlook.com>
2025-01-14 18:12:46 -0800

    Revert "fix: failed"
    This reverts commit 83eab1a0b380d074be44f2f37b7ed86062b0081f.

--- appium/config/wdio.mock.conf.ts
+++ appium/config/wdio.mock.conf.ts
@@ -12,7 +12,6 @@ config.suites = {
   failed: [
     '../tests/specs/mock/composeEmail/CheckDraftsFunctionality.spec.ts',
     '../tests/specs/mock/composeEmail/CheckRecipientPopup.spec.ts',
-    '../tests/specs/mock/inbox/CheckRichTextEmailRendering.spec.ts',
   ],
 };
 
